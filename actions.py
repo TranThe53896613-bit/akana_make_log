4ad6b86df75edc8b17a40ed4123b3c767f0cd8b3
kalculata <nprincehuzaifa@gmail.com>
2021-01-02 13:41:22 +0200

    refactor of all project; add addLog class

--- actions.py
+++ actions.py
@@ -3,3 +3,17 @@ CREATE = 2
 DELETE = 3
 MODIFY = 4
 BUY = 5
+
+def get_action_name(code):
+    if code == 1:
+        return "ADD"
+    elif code == 2:
+        return "CREATE"
+    elif code == 3:
+        return "DELETE"
+    elif code == 4:
+        return "MODIFY"
+    elif code == 5:
+        return "BUY"
+    else:
+        raise ValueError(f"code action {code} is invalid")
